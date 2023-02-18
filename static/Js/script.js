7ed2210bb9f98f67ac298b1b43efa42514536136
Zohaib <zrana1214@gmail.com>
2023-02-18 14:19:25 -0500

    better scrollbar

--- static/Js/script.js
+++ static/Js/script.js
@@ -5,13 +5,14 @@ var theme_off = true
 btn.onclick =  function(){
     // console.log(`${theme_off}`)
     if(theme_off === true){
-        document.body.style.backgroundColor = 'rgb(26, 37, 61)'
+        // document.body.style.backgroundColor = 'rgb(26, 37, 61)'
+        document.documentElement.style.setProperty('--BG_color', 'rgb(26, 37, 61)');
         document.documentElement.style.setProperty('--main_color', 'rgb(210, 0, 0)');
 
 
         theme_off = false
     } else{
-        document.body.style.backgroundColor = 'rgb(34, 34, 34)'
+        document.documentElement.style.setProperty('--BG_color', 'rgb(34, 34, 34)');
         document.documentElement.style.setProperty('--main_color', 'rgb(207, 207, 207)');
 
         theme_off = true
