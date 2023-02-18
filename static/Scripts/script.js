3da0f795e9027b7b32ae673892c3168ac63efb2f
Zohaib <zrana1214@gmail.com>
2023-02-17 22:21:43 -0500

    spider mode background and title done

--- static/Scripts/script.js
+++ static/Scripts/script.js
@@ -1,13 +1,20 @@
 const btn = document.getElementById('theme_switch')
+const title = document.getElementById('title')
 var theme_off = true
 
 btn.onclick =  function(){
     // console.log(`${theme_off}`)
     if(theme_off === true){
         document.body.style.backgroundColor = 'rgb(26, 37, 61)'
+        document.documentElement.style.setProperty('--main_color', 'rgb(210, 0, 0)');
+
+
         theme_off = false
     } else{
         document.body.style.backgroundColor = 'rgb(34, 34, 34)'
+        document.documentElement.style.setProperty('--main_color', 'rgb(207, 207, 207)');
+
         theme_off = true
     }
 }
+// rgb(210, 0, 0)
